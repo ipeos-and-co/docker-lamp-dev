7e6d1ded8269850d99fdc2610102ed70b4461c9b
Laurent Vergerolle - IPEOS <laurent@ipeos.com>
2021-10-28 16:49:28 -0400

    php7.4

--- php7/entrypoint.sh
+++ php7/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-cp /etc/php/7.0/apache2/php.ini /tmp/php.ini.tpl
+cp /etc/php/7.4/apache2/php.ini /tmp/php.ini.tpl
 sed -i "s/^error_reporting \?=.*/error_reporting = \${PHP_ERROR_REPORTING}/g" /tmp/php.ini.tpl
 sed -i "s/^display_errors \?=.*/display_errors = \${PHP_DISPLAY_ERRORS}/g" /tmp/php.ini.tpl
 sed -i "s/^upload_max_filesize \?=.*/upload_max_filesize = \${PHP_UPLOAD_MAX_FILE_SIZE}/g" /tmp/php.ini.tpl
@@ -10,7 +10,7 @@ sed -i "s/^post_max_size \?=.*/post_max_size = \${PHP_POST_MAX_SIZE}/g" /tmp/php
 sed -i "s/^expose_php \?=.*/expose_php = \${PHP_EXPOSE_PHP}/g" /tmp/php.ini.tpl
 sed -i "s|^;date\.timezone \?=.*|date\.timezone = \${PHP_TIMEZONE}|g" /tmp/php.ini.tpl
 
-envsubst < /tmp/php.ini.tpl > /etc/php/7.0/apache2/php.ini
+envsubst < /tmp/php.ini.tpl > /etc/php/7.4/apache2/php.ini
 rm /tmp/php.ini.tpl
 
 if [ "$ENABLE_HTACCESS" = On ]; then
